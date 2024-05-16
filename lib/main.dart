d09a04fddd485e29f523946ad165c8ec9b4c3823
mayank <mayankrajput40558@gmail.com>
2024-05-16 23:18:16 +0530

    fixed ui issue

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.light(),
       debugShowCheckedModeBanner: false,
       darkTheme: ThemeData(
         fontFamily: GoogleFonts.poppins().fontFamily,
